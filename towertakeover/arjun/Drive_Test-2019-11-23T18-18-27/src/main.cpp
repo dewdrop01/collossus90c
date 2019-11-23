31fdb9d6304d63bab5613f7791d92e48a22156e8
ArjunIyer000 <48413931+ArjunIyer000@users.noreply.github.com>
2019-11-23 13:20:54 -0500

    Drive Test Program
    Drive Test Program
    -changed intake speed
    -changed ramp motor speed

---
+++ towertakeover/arjun/Drive_Test-2019-11-23T18-18-27/src/main.cpp
@@ -0,0 +1,193 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Clawbot (Drivetrain) - Template                           */
+/*                                                                            */
+/*    Name:                                                                   */
+/*    Date                                                                    */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2            
+// LeftIntakeMotor      motor         5               
+// LeftArmMotor         motor         3               
+// RightArmMotor        motor         4               
+// RightIntakeMotor     motor         6               
+// Controller1          controller                    
+// RampMotor            motor         7               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  LeftIntakeMotor.setVelocity(100,percent);
+  RightIntakeMotor.setVelocity(100,percent); 
+
+  RampMotor.setVelocity(50
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ,percent);
+};
